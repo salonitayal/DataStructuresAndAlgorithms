d3022ab112d3dc73366b4c29257bdb9fec9eb7ed
salonitayal <salonitayal7164@gmail.com>
2021-01-06 17:33:13 +0530

    added files

--- Codechef-solutions/JAN21/point-of-impact.cpp
+++ Codechef-solutions/JAN21/point-of-impact.cpp
@@ -8,65 +8,27 @@ int main()
     {
         int n, k, x, y; cin >> n >> k >> x >> y;
 
-        if(x == y)
-        {
-            x = n;
-            y = n;
-        }
+        if(x == y)cout << n << " " << n << endl;
         else
         {
-            if(k > n)
-            {
-                k %= 5;
-                ++k;
-            }
-            int i = 0;
-            if(i < k)
-            {
-                int d = abs(x-y);
-                if(i == 1 || k == n)
-                {
-                    if(x > y)
-                    {
-                        x = n;
-                        y = n - d;
-                    }
-                    else
-                    {
-                        y = n;
-                        x = n - d;
-                    }
-                    if(k == n)i = n+1;
-                }
-                ++i;
-                if(i < k)
-                {
-                    swap(x, y);
-                    ++i;
-                    if(i < k)
-                    {
-                        int e = abs(x - y);
-                        if(x > y)
-                        {
-                            x  = e;
-                            y = 0;
-                        }
-                        else
-                        {
-                            y = e;
-                            x = 0;
-                        }
-                        ++i;
-                        if(i < k)
-                        {
-                            swap(x, y);
-                        }
-                        
-                    }
-                }
-            }
+           vector<pair<int, int>> v;
+           if(x < y)
+           {
+               v.push_back(make_pair(x-y+n, n));
+               v.push_back(make_pair(n, n-y+x));
+               v.push_back(make_pair(y-x, 0));
+               v.push_back(make_pair(0, y-x));
+           }
+           else
+           {
+               v.push_back(make_pair(n, y+n-x));
+               v.push_back(make_pair(y+n-x, n));
+               v.push_back(make_pair(0, x-y));
+               v.push_back(make_pair(x-y, 0));
+           }
+           int x = (k-1)%4;
+           cout << v[x].first << " " << v[x].second << endl;
         }
-        cout << x << " " << y << endl;
     }
     return 0;
 }
