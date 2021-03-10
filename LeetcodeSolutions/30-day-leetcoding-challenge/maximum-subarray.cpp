e4572cb831a51f3946ff926382c8a51c96c167dd
salonitayal <salonitayal7164@gmail.com>
2021-03-11 01:51:31 +0530

    added leetcodesolution

---
+++ LeetcodeSolutions/30-day-leetcoding-challenge/maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_current, max_global;
+        max_current = max_global = nums[0];
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            max_current = max(nums[i], max_current+nums[i]);
+            if(max_current > max_global)max_global = max_current;
+        }
+        return max_global;
+    }
+};
