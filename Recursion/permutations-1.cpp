41c1e9f2c4735b9cca373c383e4de88916106ed1
salonitayal <salonitayal7164@gmail.com>
2021-09-29 00:24:41 +0530

    added files

--- Recursion/permutations-1.cpp
+++ Recursion/permutations-1.cpp
@@ -1,41 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-/**
- * This is different way of approaching permutation based problems.
- * Although the solution is confusing for this, but its worth doing.
-**/
-void permutations(int cb, int tb, vector<int> itms, int ssf, int ts, string asf)
+int c=0;
+void solve(int n, int itm, int cnt, vector<int> x)
 {
-    if(cb > tb)
+    if(cnt == itm+1)
     {
-        if(ssf == ts)
+        for(int i=0; i<n; i++)cout << x[i];
+        cout << endl;
+        return;
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(x[i] == 0)
         {
-            cout << asf << endl;
-            return;
+            x[i] = cnt ;
+            solve(n, itm, cnt+1, x);
+            x[i] = 0;
         }
     }
-    for(int i=0; i<ts; i++)
-    {
-        if(itms[i] == 0)// use ni hua
-        {
-            itms[i] = 1;
-            /**
-             * current_box = cb is incremented, as now we have to choose from tb-cb,
-             * selection_so_far = ssf is incremented, as we have to select from ts-ssf boxes now
-            **/
-            permutations(cb+1, tb, itms, ssf+1, ts, asf + to_string(i+1)); 
-            itms[i] = 0;
-        }
-    }
-    permutations(cb+1, tb, itms, ssf, ts, asf+'0');
+    
+
 }
+
 int main()
 {
     int n, itm; cin >> n >> itm;
     vector<int> x(n);
     for(int i=0; i<n; i++)x[i] = 0;
-    permutations(1, n, x, 0, itm, "");
+    solve(n, itm, 1, x);
     return 0;
 }
 
